ef08e4e0d1b5015a255d922a5679055efa556991
Nikita Zhevlakov <rbkart@mail.ru>
2024-05-16 21:46:43 +0400

    Creating the main method

--- gallows.java
+++ gallows.java
@@ -1,2 +1,27 @@
-public class gallows {
+import java.util.Scanner;
+
+class gallows {
+    public static void main(String[] args) {
+
+
+        Scanner decision = new Scanner(System.in);
+        System.out.println("Для начала игры введите Да или Нет.");
+        String startVar = decision.nextLine();
+
+        if (startVar.equals("Да")) {
+            makeWord();
+        } else if (startVar.equals("Нет")) {
+            System.out.println("Игра окончена. До встречи.");
+        } else {
+            System.out.println("Некорректное значение. Повторите ввод.");
+        }
+    }
+
+
+
+    public static void makeWord() {
+        System.out.println("start");
+    }
+
+
 }
