f965624db9b566f666e77244471fc838080912b2
Nikita Zhevlakov <rbkart@mail.ru>
2024-05-17 20:33:32 +0400

    Creating a game menu

--- gallows.java
+++ gallows.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 class gallows {
     public static void main(String[] args) {
-
-
+        boolean counter = false;
+    do {
         Scanner decision = new Scanner(System.in);
         System.out.println("Для начала игры введите Да или Нет.");
         String startVar = decision.nextLine();
 
         if (startVar.equals("Да")) {
             makeWord();
+            break;
         } else if (startVar.equals("Нет")) {
             System.out.println("Игра окончена. До встречи.");
+            break;
         } else {
             System.out.println("Некорректное значение. Повторите ввод.");
+            counter = true;
         }
+        } while(counter == true);
     }
 
 
