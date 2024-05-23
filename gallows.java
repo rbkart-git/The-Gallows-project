8a2daf66288f7a3c166277d470e56f5b7b08098b
Nikita Zhevlakov <rbkart@mail.ru>
2024-05-23 21:52:10 +0400

    Function startGame

--- gallows.java
+++ gallows.java
@@ -1,40 +1,61 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 class gallows {
 
+    /*меню игры*/
     public static void main(String[] args) throws FileNotFoundException {
-        boolean counter = false;
-    do {
+        boolean flag = false;
         Scanner decision = new Scanner(System.in);
-        System.out.println("Для начала игры введите Да или Нет.");
-        String startVar = decision.nextLine();
-        if (startVar.equals("Да")) {
-            startGame();
-            break;
-        } else if (startVar.equals("Нет")) {
-            System.out.println("Игра окончена. До встречи.");
-            break;
-        } else {
-            System.out.println("Некорректное значение. Повторите ввод.");
-            counter = true;
-        }
-        } while(counter == true);
+        do {
+            System.out.println("Для начала игры введите Да или Нет.");
+            String startVar = decision.nextLine();
+            if (startVar.equals("Да")) {
+                startGame();
+                break;
+            } else if (startVar.equals("Нет")) {
+                System.out.println("Игра окончена. До встречи.");
+                break;
+            } else {
+                System.out.println("Некорректное значение. Повторите ввод.");
+                flag = true;
+            }
+        } while(flag = true);
+        decision.close();
     }
 
+    /*начало игры*/
     public static void startGame() throws FileNotFoundException {
         File textFile = new File("D:\\Repositories\\The Gallows project\\dictionary.txt");
         Scanner readTextFile = new Scanner(textFile);
         ArrayList<String> dictionary = new ArrayList<String>();
+
         while(readTextFile.hasNextLine()) {
             dictionary.add(readTextFile.nextLine());
         }
         readTextFile.close();
+
+        int errorCounter = 0; // счётчик ошибок
+        ArrayList<String> lettersEntered = new ArrayList<>(); // сюда буду записывать буквы, которые ввёл пользователь
+
+        Random randIndex = new Random();
+        String randomElement = dictionary.get(randIndex.nextInt(dictionary.size()));
+        int wordLength = randomElement.length();
+        String mask = "*";
+        String hiddenWord = mask.repeat(wordLength);
+        if (wordLength == 1 || wordLength == 11) {
+            System.out.println("Слово состоит из " + wordLength + " буквы");
+        } else if (wordLength > 1 && wordLength < 11 || wordLength > 11 || wordLength < 21) {
+            System.out.println("Слово состоит из " + wordLength + " букв");
+        }
+        System.out.println("Ошибок допущено: " + errorCounter);
+        System.out.println("Введённые ранее буквы: " + lettersEntered);
         System.out.println("---------");
-        System.out.println("|      |");
+        System.out.println("|       |");
         System.out.println("|");
         System.out.println("|");
         System.out.println("|");
@@ -43,62 +64,43 @@ class gallows {
         System.out.println("|");
         System.out.println("|");
         System.out.println("---------");
-        randomWordSelection(dictionary);
+
+        Scanner letterEntered = new Scanner(System.in);
+        System.out.println("Введите букву");
+        String letter = letterEntered.nextLine();
+        letterEntered.close();
+
+        do {
+            boolean flag = false;
+            String numbers = "0123456789";
+            int value = numbers.indexOf(letter);
+            if (value != -1) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+
+            /* реализовать if...else на основе сравнение символа строки со строками "0123456789" и "абвгдезжикмино   и т.д." "abcdefghi  и т.д."
+            * это должно быть в теле цикла и при вводе числа, специального символа или английской буквы выводиться сообщение о неккоректном вводе
+            * и повторяться ввод;
+            *   */
 
 
 
-        // отобразить счётчик ошибок и введённые ранее буквы
-        // предложить ввод буквы
+        } while(true);
+
+
+
+
+        bodyOfTheGame(errorCounter, lettersEntered, letter, randomElement, hiddenWord);
     }
 
-    public static void randomWordSelection(ArrayList<String> dictionary) {
-        Random randIndex = new Random(); //
-        String randomElement = dictionary.get(randIndex.nextInt(dictionary.size())); //выбираю случайное слово из словаря
+    public static void bodyOfTheGame(int errorCounter, ArrayList<String> lettersEntered, String letter, String randomElement, String hiddenWord) {
 
-        int wordLength = randomElement.length(); // получаю длину слова
-        int minIndex = 0; // стартовый индекс слова
-        int maxIndex = wordLength; // конечный индекс слова
-        int randomMask1 = (int)(Math.random()*(maxIndex - minIndex + 1) + minIndex); // первый случайный символ, который хочу открыть
-        int randomMask2 = (int)(Math.random()*(maxIndex - minIndex + 1) + minIndex); // второй случайный символ, который хочу открыть
-        char mask = '*'; // символ, которым буду скрывать слово
 
-        makeWord(randomElement, wordLength, minIndex, maxIndex, randomMask1, randomMask2, mask);
-
-        System.out.println(makeWord(randomElement, wordLength, minIndex, maxIndex, randomMask1, randomMask2, mask));
 
 
     }
-
-    public static String makeWord(String randomElement, int wordLength, int minIndex, int maxIndex, int randomMask1, int randomMask2, char mask)  {
-
-    StringBuilder hiddenWord = new StringBuilder(randomElement); // в этом классе хочу поменять символы слова на символ из переменной mask
-    for (int count = minIndex; count <= maxIndex; count++) {
-        hiddenWord.charAt(mask); // заменяю символы слова на символ из mask
-    }
-    return hiddenWord.toString(); // возвращаю скрытую маской строку
-
-
-
-
-        // System.out.println(randomElement + " " +  wordLength + " " + randomMask1 + " " + randomMask2);
-
-
-
-
-        // сюда передал выбранное слово
-        // в символьную переменную нужно положить символ, которым будет накладываться маска
-        // создать переменные, в которые положить начальный индекс строки (слова) и конечный индекс
-        // циклом for и методом setCharAt() накладывать маску на слово, но нужно найти способ выбирать случайный диапазон накладывания маски
-
-
-
-
-        // отобразить загаданное слово, наложив на него маску и открыв две случайные буквы
-
-    }
-
-
-
 
 
 
