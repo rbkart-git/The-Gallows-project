3ddeaad1e068f86ea7a8fb5df7a662068378091c
Nikita Zhevlakov <rbkart@mail.ru>
2024-05-28 21:02:00 +0400

    commit

--- gallows.java
+++ gallows.java
@@ -1,100 +1,75 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class gallows {
+    public static void main(String[] args) {
+        // startgame();
+    }
 
-    /*меню игры*/
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void startGame() {
+        // старт игры на основе цикла do while и if else
+    }
 
-        /* Меню игры */
-        boolean flag = false;
-        Scanner decision = new Scanner(System.in);
-        do {
-            System.out.println("Для начала игры введите Да или Нет.");
-            String startVar = decision.nextLine();
-            if (startVar.equals("Да")) {
-                startGame();
-                break;
-            } else if (startVar.equals("Нет")) {
-                System.out.println("Игра окончена. До встречи.");
-                break;
-            } else {
-                System.out.println("Некорректное значение. Повторите ввод.");
-                flag = true;
+    public static void loadDictionary() {
+        // загрузка словаря
+        // размещение его в динамическом массиве
+    }
+
+    public static void choosingRandomWord () {
+        // выбор случайного слова из словаря
+    }
+
+    public static void hideWordMask () {
+        // наложить маску на слово
+    }
+
+    public static void createGameParameters () {
+        // показать пустой каркас виселицы
+        // создать счётчик ошибок
+        // создать динамический массив для введённых букв
+    }
+
+    public static void letterEntering () {
+        /*
+            while () {
+
+                ввод буквы
+
+                 если правильно -> openLetterCorrect
+                 если не правильно -> letterEnterIncorrect
             }
-        } while(flag = true);
-        decision.close();
+         */
+
     }
 
-    /*начало игры*/
-    public static void startGame() throws FileNotFoundException {
-
-        /* Создаю словарь слов */
-        File textFile = new File("D:\\Repositories\\The Gallows project\\dictionary.txt");
-        Scanner readTextFile = new Scanner(textFile);
-        ArrayList<String> dictionary = new ArrayList<String>();
-
-        while(readTextFile.hasNextLine()) {
-            dictionary.add(readTextFile.nextLine());
-        }
-        readTextFile.close();
-
-        /* Выбираю случайное слово из словаря и накладываю на него маску */
-        Random randIndex = new Random();
-        String randomElement = dictionary.get(randIndex.nextInt(dictionary.size()));
-        int wordLength = randomElement.length();
-        String mask = "*";
-        String hiddenWord = mask.repeat(wordLength);
-        if (wordLength == 1 || wordLength == 11) {
-            System.out.println("Слово состоит из " + wordLength + " буквы");
-        } else if (wordLength > 1 && wordLength < 11 || wordLength > 11 || wordLength < 21) {
-            System.out.println("Слово состоит из " + wordLength + " букв");
-        }
-        System.out.println("---------");
-        System.out.println("|       |");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("|");
-        System.out.println("---------");
-
-        /*Валидация введённого символа*/
-        boolean flag = false;
-        Scanner letterEntered = new Scanner(System.in);
-        do {
-            System.out.println("Введите букву");
-            String letter = letterEntered.nextLine();
-            Pattern patternCyrillic = Pattern.compile("[а-яА-Я]");
-            Matcher matcherCyrillic = patternCyrillic.matcher(letter);
-
-            if (matcherCyrillic.find()) {
-                comparingCharacterInWord(hiddenWord, letter);
-                break;
-            } else {
-                System.out.println("Символ введён неправильно, повторите ввод.");
-                flag = true;
-            }
-        } while(flag = true);
-        letterEntered.close();
+    public static void openLetterCorrect () {
+        // сравнить выбранное слово и введённую букву
+        // отобразить букву
+        // показать счётчик ошибок
+        // показать введённые буквы
+        // если все буквы введены правильно -> chekingEndGameVictory
     }
 
-    /*сравнение введённого символа с выбранным словом из словаря*/
-    public static void comparingCharacterInWord(String hiddenWord, String letter) {
-        int errorCounter = 0; // счётчик ошибок
-        ArrayList<String> lettersEntered = new ArrayList<>(); // сюда буду записывать буквы, которые ввёл пользователь
-        lettersEntered.add(letter);
-
-        System.out.println("Введённые символы: " + lettersEntered);
-        System.out.println("Ошибок допущено: " + errorCounter);
+    public static void chekingEndGameVictory () {
+        // вы победили, хотите сыграть еще?
     }
 
+    public static void letterEnterIncorrect () {
+        // добавить к виселице один элемент и показать её
+        // инкремент счётчика ошибок и его показ
+        // занести букву в список
+        // счётчик ошибок больше допустимого -> chekingEndGameLoss
+        // счётчик ошибок меньше допустимого -> letterEntering
+    }
+
+    public static void chekingEndGameLoss () {
+        // вы проиграли, хотите сыграть еще?
+    }
 }
+
+
+
+
+
+
+
+
+
